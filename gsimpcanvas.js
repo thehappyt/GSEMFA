d5caf59b25ec585ee265dca5347445784d2f7307
thehappyt <thompson.adams@gmail.com>
2014-08-21 01:37:29 -0700

    Update gsimpcanvas.js

--- gsimpcanvas.js
+++ gsimpcanvas.js
@@ -18,6 +18,7 @@
     Object.defineProperty(canvas.prototype, "grids", {configurable: false, enumerable: true, writable: false,
         value: { N: 15, d: 1, shaftwidth: 0.075, loff: 0.0, eoff: 0.1, voff: -0.2 }
     })
+    Object.defineProperty(canvas.prototype, "grid", {configurable: false, enumerable: true, writable: true, value: null })
     Object.defineProperty(canvas.prototype, "sources", {configurable: false, enumerable: true, writable: false,
         value: { chargesize: 0.75, qoff: 0.0, loff: 0.0, k0: 1.0 }
     })
