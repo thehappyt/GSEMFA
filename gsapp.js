42cf35772cf068cfc572105686cbdf2d71b5e1d2
thehappyt <thompson.adams@gmail.com>
2014-08-21 01:27:22 -0700

    Update gsapp.js

--- gsapp.js
+++ gsapp.js
@@ -141,8 +141,8 @@
             var b = $('<button>'), text=opt.text||false;
             if (opt.title) { opt.title = opt.title + ''; b.attr({ title: opt.title }); }
             //if (opt.text) { opt.text = opt.text + ''; b.text( opt.text ); } //(NOT WORKING YET  b.css("width", "50px"))
-            b.button({ icons: { primary: "gsapp-icon " + opt.icon }, text: text })
-            b.removeClass('ui-icon')
+            b.button({ icons: { primary: "gsapp-icon " + opt.icon }, text: text }) // REQUIRES opt.icon for now!
+            b.children("span").removeClass('ui-icon')
             b.click( function() { $(this).blur(); });
             this.canvas.iconbar.append(b);
             return b;
